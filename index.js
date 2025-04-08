81e8bfe13719b95ec11dd3183c1b55bdf5e57353
david-rai <davidkhaling123@gmail.com>
2025-04-08 14:58:44 +0545

    fs module is done

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs=require("fs")
+
+// const data=fs.readFileSync("./demo.txt","utf-8")
+// console.log(data)
+
+// fs.readFile("./demo.txt","utf8",(err,data)=>{
+//     if(err){
+//         return err
+//     }
+
+//     console.log(data)
+//     return console.log("sucessfully done")
+// })
+
+// fs.appendFileSync("./demo.txt","\n  amother line is added")
+
+// fs.appendFile('./demo.txt',"hey there",(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     return console.log("sucessfully apppended")
+// })
+
+
+// fs.unlinkSync("./demo.txt")
+
+// fs.unlink("./demo.txt",(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+// })
+
+
+// fs.writeFileSync("./demo.txt","hey there")
+
+// fs.copyFileSync("./demo.txt","./second.txt")
+// fs.unlinkSync('./second.txt')
