f12d81dff64f3026345c098b13044895c9bf247b
david-rai <davidkhaling123@gmail.com>
2025-04-09 13:49:30 +0545

    rest api 80%

--- index.js
+++ index.js
@@ -1,27 +1,120 @@
-const express=require('express')
-const app=express()
-require('dotenv').config()
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const fs = require("fs");
 
-// it converts the fetch or axios json into the js objects
-app.use(express.json())
+// Middleware to parse JSON bodies
+app.use(express.json());
 
-//it serves the files like html,css,js and images to the client 
-app.use(express.static('public'))
+// Middleware to serve static files from 'public' directory
+app.use(express.static("public"));
 
-//it helps to understand the form plain data into readable format such as js 
-app.use(express.urlencoded({extended:true}))
+// Middleware to parse URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
+// Home page route
+app.get("/", (req, res) => {
+  res.send("Hello, this is the home page");
+});
 
-    res.send("home pages")
+// Users route (GET /users)
+app.get("/users", (req, res) => {
+  fs.readFile("./data.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Something went wrong");
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+//GET/users/:id
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  fs.readFile("./data.json", "utf8", (err, data) => {
+    if (err) {
+      res.send(err);
+    }
+
+    const datas = JSON.parse(data);
+    const filtered = datas.filter((d) => d.id == id);
+
+    res.json(filtered);
+  });
+});
+
+//POST/users
+app.post('/users',(req,res)=>{
+const Userdata=req.body
+
+fs.readFile('./data.json','utf8',(err,data)=>{
+   if(err){
+    res.send(err)
+   }
+
+     //adding the data
+const finalData=JSON.parse(data)
+finalData.push(Userdata)   
+   
+//writing into the user
+fs.writeFile("./data.json",JSON.stringify(finalData,null,2),(err)=>{
+    if(err){
+        res.send(err)
+    }
+res.json({msg:"successfully added",finalData})    
 })
 
-app.post('/add',(req,res)=>{
-const data=req.body
+})
 
-res.json({message:"successfully added",data})
 
 })
 
-const port=process.env.PORT
-app.listen(port,()=> console.log("server starting...."))
+//PUT/users/:id
+app.put('/users/:id',(req,res)=>{
+    const newData=req.body
+    const userId=req.params.id
+
+    fs.readFile("./data.json",'utf8',(err,data)=>{
+        if(err){
+            res.json(err)
+        }
+
+       const finalData=JSON.parse(data)
+       const userIndex=finalData.findIndex((user)=> user.id == userId) 
+       finalData[userIndex]={...finalData[userIndex],...newData}
+
+              //writing the data into database
+       fs.writeFile("./data.json",JSON.stringify(finalData,null,2),(err)=>{})
+     res.json({msg:"sucessfully updated",newData})
+        
+    })
+
+
+
+})
+
+//DELETE/users/:id
+app.delete('/users/:id',(req,res)=>{
+    const userId=req.params.id
+
+    fs.readFile('./data.json','utf8',(err,data)=>{
+        if(err){
+            res.json(err)
+        }
+
+        const finalData=JSON.parse(data)
+        const filtered=finalData.filter(d=> d.id !== userId)
+
+         //writing into the database
+         fs.writeFile('./data.json',JSON.stringify(filtered,null,2),(err)=>{
+            if(err){
+                res.json(err)
+            }
+         res.json({msg:"sucessfully deleted",filtered})
+         })
+    })
+})
+
+// Start server on a port specified in .env file
+const port = process.env.PORT;
+app.listen(port, () => console.log("Server starting...."));
